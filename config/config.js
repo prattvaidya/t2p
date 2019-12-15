45a4206e60df5973aa83f544fb263ed0edf19e91
Prathamesh Vaidya <prathamesh.vaidya@outlook.com>
2019-12-14 16:55:28 -0800

    Modifications for Heroku v1

--- config/config.js
+++ config/config.js
@@ -2,15 +2,7 @@ const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri:
-    process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    "mongodb://" +
-    (process.env.IP || "localhost") +
-    ":" +
-    (process.env.MONGO_PORT || "27017") +
-    "/points_db",
-
+  mongoUri: 'mongodb://pratt268:pratt268@ds233278.mlab.com:33278/t2p',
   email_service: 'gmail',
   email_username: 'abc@gmail.com',
   email_pwd: '123test'
