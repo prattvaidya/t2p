e854b955575a192e0dbdb4ad06035b4a4c0162c1
Prathamesh Vaidya <prathameshrvaidya@gmail.com>
2020-12-15 21:17:23 -0800

    Update mongoURI

--- config/config.js
+++ config/config.js
@@ -2,14 +2,7 @@ const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri:
-    process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    "mongodb://" +
-    (process.env.IP || "localhost") +
-    ":" +
-    (process.env.MONGO_PORT || "27017") +
-    "/points_db",
+  mongoUri: "mongodb+srv://pratt268:pratt268@t2p.iljc9.mongodb.net/t2p?retryWrites=true&w=majority",
 
   email_service: 'gmail',
   email_username: 'abc@gmail.com',
