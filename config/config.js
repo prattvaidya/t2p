2b4f2079d5ffc17e84c84519a148630f4336e051
Prathamesh Vaidya <prathameshrvaidya@gmail.com>
2020-12-15 22:02:28 -0800

    use env for mongouri

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: "mongodb+srv://pratt268:pratt268@t2p.iljc9.mongodb.net/t2p?retryWrites=true&w=majority",
+  mongoUri: "mongodb+srv://"+process.env.UPASS+"@t2p.iljc9.mongodb.net/t2p?retryWrites=true&w=majority",
 
   email_service: 'gmail',
   email_username: 'abc@gmail.com',
